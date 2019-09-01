b38adf1ba921f293af37f96c6008fdf407402694
Mario Maureira <mario_swin@outlook.com>
2019-08-31 23:51:03 -0400

    add config to babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+
+    }
+}
